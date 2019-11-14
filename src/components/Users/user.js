f6c951d099a6194dd202cde63d76061a908868d6
Arjun Raja <arjunraja@cybrilla.com>
2019-11-14 07:14:06 +0530

    add about page

--- src/components/Users/user.js
+++ src/components/Users/user.js
@@ -1,6 +1,7 @@
-import React, { Component } from 'react'
+import React, { Component, Fragment  } from 'react'
 import PropTypes from 'prop-types'
 import Spinner from '../layout/spinner';
+import { Link } from 'react-router-dom';
 
 class User extends Component {
     componentDidMount() {
@@ -16,6 +17,7 @@ class User extends Component {
     }
     render() {
         const {
+            company,
             name,
             login,
             avatar_url,
@@ -30,13 +32,44 @@ class User extends Component {
         } = this.props.user;
 
         return (
-        <div> 
-            <h1>
-            Hello {name} we know you as {login}
-            </h1> 
-            <h2> You live in {location} </h2>
-
-        </div>
+        <Fragment> 
+            <Link to='/' className='btn btn-light'>Back to search</Link>
+            Hireable: {''}
+            {hireable? <i className='fa fa-check text-success' /> : <i className='fa fa-times-circle text-danger' /> } 
+            <div className="card grid-2">
+             <div className='all-center'>
+                <img src={avatar_url} className='round-img' alt='' style={{width: '150px'}}/>
+                <h1>Name: {name}</h1>
+                <div className='badge badge-dark'>Location: {location}</div>
+             </div>
+              <div>
+                {bio && <><h1>Bio</h1> {bio}</>}
+                <a href={html_url} className='btn btn-dark my-1'>Visit github profile</a>
+                <Fragment>
+                    <ul>
+                        <li>
+                            <strong>Username:</strong> {login}
+                        </li>
+                        {company && <li>
+                            <strong>Company: </strong> {company}
+                        </li>}
+                        {blog && <li>
+                            <div>
+                            <strong>blog: </strong> 
+                            {blog}
+                            </div>
+                        </li>}
+                    </ul>
+                </Fragment>
+              </div>
+            </div>
+            <div className='card text-center'>
+                    <div className='badge badge-primary'>Followers: {followers}</div>
+                    <div className='badge badge-success'>Following: {following}</div>
+                    <div className='badge badge-danger'>Public Repos: {public_repos}</div>
+                    <div className='badge badge-dark'>empty</div>
+            </div>
+        </Fragment>
         )
     }
 }
